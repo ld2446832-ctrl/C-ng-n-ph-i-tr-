6fd888bf2b80514cc78c49a13276f4022b53d9ff
Đào Ngọc Linh <ld2446832@gmail.com>
2026-06-06 15:34:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using CongNoPhaiTra.Data;
+using CongNoPhaiTra.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Cấu hình kết nối SQL Server LocalDB
+builder.Services.AddDbContext<AccountingDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
